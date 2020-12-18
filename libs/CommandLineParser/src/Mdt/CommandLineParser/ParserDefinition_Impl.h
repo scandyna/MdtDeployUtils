26461bda604aacfb6def203f328961fceda4c124
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-12-18 07:04:36 +0100

    CommandLine parser: work on breakText()

--- libs/CommandLineParser/src/Mdt/CommandLineParser/ParserDefinition_Impl.h
+++ libs/CommandLineParser/src/Mdt/CommandLineParser/ParserDefinition_Impl.h
@@ -95,12 +95,25 @@ namespace Mdt{ namespace CommandLineParser{
       }
 
       /*
-       * We are just before max length and found no breakable char
-       * cut here, the char will go to the next line,
-       * and a - will be here later
+       * Index is at the last position allowed by maxLength
+       *
+       * Possible cases:
+       *  1) we also are reaching the en of the string: 'AB'
+       *                                                  ^
+       *  2) the next char is a beakable: 'AB '
+       *                                    ^
+       *  3) the next char is not a breakable: 'ABC'
+       *                                         ^
        */
       if( (index == maxLength-1) && (lastBreakable == last) ){
-//         ++first;
+        auto next = first+1;
+        if(next == last){
+          return last;
+        }
+        if( isBreackableChar(*next) ){
+          return next;
+        }
+
         return first;
       }
 
@@ -111,7 +124,67 @@ namespace Mdt{ namespace CommandLineParser{
     return first;
   }
 
-  /*! \brief
+  /*! \brief Returns a string from \a text breaked so that the length of each line is no longer than \a maxLength
+   *
+   * \pre \a leftPadLength must be >= 0
+   * \pre \a maxLength must be > 1
+   */
+  static
+  QString breakText_2(const QString & text, int leftPadLength, int maxLength)
+  {
+    /*
+     * First attempt was to modify text inplace.
+     * This is not easy to get right while iterating over the text.
+     * With QString, we also have to care about the iterator problem,
+     * see https://doc.qt.io/qt-5/containers.html#implicit-sharing-iterator-problem .
+     * Also, while modifying text, memory allocations and copies will occur anyway.
+     * (and also: accessing a non const iterator of QString can also produce a deep copy).
+     */
+
+    assert( leftPadLength >= 0 );
+    assert( maxLength > 1 );
+
+    qDebug() << "breakText(): " << text;
+    
+    QString result;
+    const QString padding = QString( leftPadLength, QLatin1Char(' ') );
+
+    auto first = text.cbegin();
+    const auto last = text.cend();
+
+    while(first != last){
+      const auto breakPoint = findBreakPoint(first, last, maxLength);
+      const int posInText = std::distance(text.cbegin(), first);
+      const int charsToCopy = std::distance(first, breakPoint);
+      
+      qDebug() << " - At end: " << (breakPoint == last);
+      
+      qDebug() << " - posInText: " << posInText;
+      qDebug() << " - charsToCopy: " << charsToCopy;
+      
+      first += charsToCopy;
+
+      if(breakPoint == last){
+        result += text.mid(posInText, charsToCopy);
+      }else{
+        qDebug() << " - found break point " << *breakPoint;
+        if( isBreackableChar(*breakPoint) ){
+          /*
+           * Note: we don't have to handle the case of the breakPoint being a EOL,
+           * because we skip the break point.
+           */
+          result += text.mid(posInText, charsToCopy) + QLatin1Char('\n') + padding;
+          ++first;
+        }else{
+          result += text.mid(posInText, charsToCopy) + QLatin1String("-\n") + padding;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  /*! \brief \todo document
    *
    * \pre \a leftPadLength must be >= 0
    * \pre \a maxLength must be > 1
@@ -122,13 +195,67 @@ namespace Mdt{ namespace CommandLineParser{
     assert( leftPadLength >= 0 );
     assert( maxLength > 1 );
 
+    text = breakText_2(text, leftPadLength, maxLength);
+    return;
+
+    /** \todo Modifying QString is risky + will probably produce a deep copy
+     *
+     * Mybe pass text as const QString and build a resulting string to return ?
+     *
+     * NOTE: forget vector<QChar>, this is a copy anyway
+     *
+     * Or, again: index access
+     *
+     * To avoid API change:
+     * make a function that takes a const QString and returns a QString,
+     * then call it from here (avoids changing all the tests for sandbox)
+     *
+     * text = breakText_2(text);
+     * 
+     */
+    
+
     qDebug() << "breakText(): " << text;
 
+    /** \todo Use non const iterator ( text.begin() ),
+     *   then we could use iterators to replace some part
+     *   (f.ex. at breack-point)
+     *
+     * NOTE: remeber: iterators invalidation !
+     *
+     * OR: keep as is..
+     *
+     * Should work with index
+     */
+    auto first = text.cbegin();
+    const auto last = text.cend();
+
+      /// \todo After insert/modify, get the iterators again, all ! (COW!)
+      /// Review this: file:///usr/share/qt5/doc/qtcore/containers.html#
+      ///
+      /// NOTE: once detached, but not copied, is there any problem ? Think not !
+      ///   --> Also, if passd text is allready detached, what happens ?!!!?
+
+    /*
+    while(first != last){
+      const auto breakPoint = findBreakPoint(first, last, maxLength);
+      if( breakPoint == text.cend() ){
+        return;
+      }
+      
+      qDebug() << " - found break point " << *breakPoint;
+      
+      first = breakPoint;
+
+    }
+    */
+    
     const auto breakPoint = findBreakPoint(text.cbegin(), text.cend(), maxLength);
     if( breakPoint == text.cend() ){
       return;
     }
 
+    /// \todo below code should become a function
     const int index = std::distance( text.cbegin(), breakPoint );
     
     qDebug() << " - found break point " << *breakPoint << " at index " << index;
@@ -137,7 +264,7 @@ namespace Mdt{ namespace CommandLineParser{
     
     /// \todo index checking etc..
     if( isBreackableChar(*breakPoint) ){
-      text.replace( index, 1, QLatin1Char('\n') );
+      text.replace( index, 1, QLatin1Char('\n') );  /// And is not EOL !
       text.insert( index+1, padding );
     }else{
       text.insert( index, QLatin1String("-\n") + padding );
