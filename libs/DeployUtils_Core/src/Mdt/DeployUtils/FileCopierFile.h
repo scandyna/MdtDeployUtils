4924fb4b77f126b3880ae324c86750c0b70d4ed7
Philippe Steinmann <philippe.steinmann@gmail.com>
2022-11-14 06:28:42 +0100

    Create FileCopierFile

---
+++ libs/DeployUtils_Core/src/Mdt/DeployUtils/FileCopierFile.h
@@ -0,0 +1,89 @@
+/****************************************************************************
+ **
+ ** MdtDeployUtils - A C++ library to help deploy C++ compiled binaries
+ **
+ ** Copyright (C) 2022-2022 Philippe Steinmann.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_DEPLOY_UTILS_FILE_COPIER_FILE_H
+#define MDT_DEPLOY_UTILS_FILE_COPIER_FILE_H
+
+#include "mdt_deployutilscore_export.h"
+#include <QFileInfo>
+#include <vector>
+
+namespace Mdt{ namespace DeployUtils{
+
+  /*! \brief Represents a file with its source and destination paths
+   *
+   * \sa FileCopier
+   */
+  class MDT_DEPLOYUTILSCORE_EXPORT FileCopierFile
+  {
+   public:
+
+    /*! \brief Set the source file info
+     *
+     * \pre \a file must have its absolute file path set
+     * \sa doc of QFileInfo::absoluteFilePath()
+     */
+    void setSourceFileInfo(const QFileInfo & file) noexcept;
+
+    /*! \brief Get the source file info
+     */
+    const QFileInfo & sourceFileInfo() const noexcept
+    {
+      return mSourceFileInfo;
+    }
+
+    /*! \brief Set the destination file info
+     *
+     * \pre \a file must have its absolute file path set
+     * \sa doc of QFileInfo::absoluteFilePath()
+     */
+    void setDestinationFileInfo(const QFileInfo & file) noexcept;
+
+    /*! \brief Get the destination file info
+     */
+    const QFileInfo & destinationFileInfo() const noexcept
+    {
+      return mDestinationFileInfo;
+    }
+
+    /*! \brief Mark this file as been copied
+     */
+    void setAsBeenCopied() noexcept
+    {
+      mHasBeenCopied = true;
+    }
+
+    /*! \brief Returns true if source file has been copied to the destination file
+     */
+    bool hasBeenCopied() const noexcept
+    {
+      return mHasBeenCopied;
+    }
+
+   private:
+
+    bool mHasBeenCopied = false;
+    QFileInfo mSourceFileInfo;
+    QFileInfo mDestinationFileInfo;
+  };
+
+}} // namespace Mdt{ namespace DeployUtils{
+
+#endif // #ifndef MDT_DEPLOY_UTILS_FILE_COPIER_FILE_H
---
+++ libs/DeployUtils_Core/tests/src/FileCopierFileTest.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************
+ **
+ ** MdtDeployUtils - A C++ library to help deploy C++ compiled binaries
+ **
+ ** Copyright (C) 2022-2022 Philippe Steinmann.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#include "catch2/catch.hpp"
+#include "Catch2QString.h"
+#include "Mdt/DeployUtils/FileCopierFile.h"
+#include <QString>
+#include <QLatin1String>
+#include <QFileInfo>
+
+using namespace Mdt::DeployUtils;
+
+TEST_CASE("set_SourceAndDestimation")
+{
+  FileCopierFile copierFile;
+  QFileInfo source;
+  QFileInfo destination;
+
+  source.setFile( QLatin1String("/tmp/source.txt") );
+  destination.setFile( QLatin1String("/tmp/destination.txt") );
+
+  copierFile.setSourceFileInfo(source);
+  copierFile.setDestinationFileInfo(destination);
+
+  REQUIRE( copierFile.sourceFileInfo().fileName() == QLatin1String("source.txt") );
+  REQUIRE( copierFile.destinationFileInfo().fileName() == QLatin1String("destination.txt") );
+}
+
+TEST_CASE("hasBeenCopied")
+{
+  FileCopierFile copierFile;
+
+  SECTION("default")
+  {
+    REQUIRE( !copierFile.hasBeenCopied() );
+  }
+
+  SECTION("set as beend copied")
+  {
+    copierFile.setAsBeenCopied();
+
+    REQUIRE( copierFile.hasBeenCopied() );
+  }
+}
