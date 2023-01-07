e5153ed677ad00ba70c194639f9111d05cec02c9
Philippe Steinmann <philippe.steinmann@gmail.com>
2023-01-07 11:02:05 +0100

    Work on build system

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os
 
 # NOTE: this recipe is only to install dependnecies.
@@ -17,9 +17,7 @@ class MdtDeployUtilsConan(ConanFile):
   # Due to a issue using GitLab Conan repository,
   # version ranges are not possible.
   # See https://gitlab.com/gitlab-org/gitlab/-/issues/333638
-  requires = "MdtApplication/0.3.5@scandyna/testing"
-  build_requires = "MdtCMakeModules/0.17.0@scandyna/testing", "Catch2/v2.13.7x@scandyna/testing"
-  generators = "cmake", "cmake_paths", "virtualenv", "CMakeDeps", "CMakeToolchain"
+  generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
   exports_sources = "apps/*", "libs/*", "cmake/*", "MdtDeployUtilsConfig.cmake.in", "CMakeLists.txt", "conanfile.py", "LICENSE.txt", "COPYING", "COPYING.LESSER"
   # If no_copy_source is False, conan copies sources to build directory and does in-source build,
   # resulting having build files installed in the package
@@ -33,3 +31,21 @@ class MdtDeployUtilsConan(ConanFile):
   def requirements(self):
     self.requires("qt/5.15.6")
     self.requires("boost/1.72.0")
+    self.requires("MdtCMakeConfig/0.0.5@scandyna/testing")
+    self.requires("MdtConsoleApplication/0.4.4@scandyna/testing")
+    # TODO: see if required once CommandLineParser is removed (if used in tests only, move to build_requirements)
+    self.requires("MdtCommandLineArguments/0.4.4@scandyna/testing")
+
+  # When using --profile:build xx and --profile:host xx ,
+  # the dependencies declared in build_requires and tool_requires
+  # will not generate the required files.
+  # see:
+  # - https://github.com/conan-io/conan/issues/10272
+  # - https://github.com/conan-io/conan/issues/9951
+  def build_requirements(self):
+    # TODO fix once issue solved
+    # Due to a issue using GitLab Conan repository,
+    # version ranges are not possible.
+    # See https://gitlab.com/gitlab-org/gitlab/-/issues/333638
+    self.tool_requires("catch2/2.13.9", force_host_context=True)
+    self.tool_requires("MdtCMakeModules/0.19.2@scandyna/testing", force_host_context=True)
