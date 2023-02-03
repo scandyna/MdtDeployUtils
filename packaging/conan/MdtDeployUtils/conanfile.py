419ccba89ab34b45999ceef5b236e513fa1a1871
Philippe Steinmann <philippe.steinmann@gmail.com>
2023-02-03 17:24:58 +0100

    Work on conan packages (GL issue #14)

--- packaging/conan/MdtDeployUtils/conanfile.py
+++ packaging/conan/MdtDeployUtils/conanfile.py
@@ -1,5 +1,7 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.errors import ConanInvalidConfiguration
+from conans import tools
 import os
 
 
@@ -8,17 +10,9 @@ class MdtDeployUtilsConan(ConanFile):
   license = "BSD 3-Clause"
   url = "https://gitlab.com/scandyna/mdtdeployutils"
   description = "Tools to help deploy C/C++ application binaries and their dependencies."
-  # TODO: see TODO.md
   settings = "os", "compiler", "build_type", "arch"
-  options = {"shared": [True, False]}
-  default_options = {"shared": True}
-  # TODO fix once issue solved
-  # Due to a issue using GitLab Conan repository,
-  # version ranges are not possible.
-  # See https://gitlab.com/gitlab-org/gitlab/-/issues/333638
-  # TODO: is Catch required here ? Only for builds with tests, probably not when building packages
-  build_requires = "MdtConsoleApplication/0.4.5@scandyna/testing","MdtCMakeModules/0.19.2@scandyna/testing","Catch2/v2.13.7x@scandyna/testing"
-  generators = "cmake", "cmake_paths", "virtualenv"
+  #generators = "cmake", "cmake_paths", "virtualenv"
+  generators = "CMakeDeps", "VirtualBuildEnv"
 
   # If no_copy_source is False, conan copies sources to build directory and does in-source build,
   # resulting having build files installed in the package
@@ -36,6 +30,29 @@ class MdtDeployUtilsConan(ConanFile):
       git = tools.Git()
       self.version = "%s" % (git.get_tag())
 
+
+  def requirements(self):
+    self.requires("MdtCMakeConfig/0.0.5@scandyna/testing")
+
+
+  # When using --profile:build xx and --profile:host xx ,
+  # the dependencies declared in build_requires and tool_requires
+  # will not generate the required files.
+  # see:
+  # - https://github.com/conan-io/conan/issues/10272
+  # - https://github.com/conan-io/conan/issues/9951
+  def build_requirements(self):
+    # TODO fix once issue solved
+    # Due to a issue using GitLab Conan repository,
+    # version ranges are not possible.
+    # See https://gitlab.com/gitlab-org/gitlab/-/issues/333638
+    self.tool_requires("MdtCMakeModules/0.19.2@scandyna/testing", force_host_context=True)
+    self.tool_requires("boost/1.72.0", force_host_context=True)
+    self.tool_requires("qt/5.15.6", force_host_context=True)
+    self.tool_requires("MdtCommandLineParser/0.0.6@scandyna/testing", force_host_context=True)
+    self.tool_requires("MdtConsoleApplication/0.4.5@scandyna/testing", force_host_context=True)
+
+
   # The export exports_sources attributes does not work if the conanfile.py is in a sub-folder.
   # See https://github.com/conan-io/conan/issues/3635
   # and https://github.com/conan-io/conan/pull/2676
@@ -47,25 +64,19 @@ class MdtDeployUtilsConan(ConanFile):
     self.copy("CMakeLists.txt", src="../../../", dst=".")
     self.copy("LICENSE*", src="../../../", dst=".")
 
-  def requirements(self):
-    # TODO: should (only) depend on MdtDeployUtilsCore and MdtCommandLineParser
-    # TODO: see if boost is required
-    self.requires("qt/5.15.6")
-    self.requires("boost/1.72.0")
 
-  def configure_cmake(self):
-    cmake = CMake(self)
-    cmake.definitions["FROM_CONAN_PROJECT_VERSION"] = self.version
-    cmake.definitions["WARNING_AS_ERROR"] = "ON"
-    cmake.definitions["CMAKE_MESSAGE_LOG_LEVEL"] = "DEBUG"
-    # TODO: should be conditional (not for Debug build). What about multi-config ?
-    cmake.definitions["BUILD_USE_IPO_LTO_IF_AVAILABLE"] = "ON"
-
-    return cmake
+  def generate(self):
+    tc = CMakeToolchain(self)
+    tc.variables["FROM_CONAN_PROJECT_VERSION"] = self.version
+    tc.variables["BUILD_APPS"] = "ON"
+    tc.variables["INSTALL_CONAN_PACKAGE_FILES"] = "OFF"
+    # TODO: should be conditional (not for Debug build). What about multi-config ? Also, seems to fail most of the time
+    #tc.variables["BUILD_USE_IPO_LTO_IF_AVAILABLE"] = "ON"
+    tc.generate()
 
 
   def build(self):
-    cmake = self.configure_cmake()
+    cmake = CMake(self)
     cmake.configure()
     cmake.build()
 
@@ -75,10 +86,13 @@ class MdtDeployUtilsConan(ConanFile):
     #cmake.install()
     self.run("cmake --install . --config %s --component MdtDeployUtils_Runtime" % self.settings.build_type)
 
+
   def package_id(self):
     del self.info.settings.compiler
     del self.info.settings.build_type
-    del self.info.options.shared
+
 
   def package_info(self):
-    self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+    self.cpp_info.set_property("cmake_find_mode", "none")
+    self.cpp_info.builddirs.append( os.path.join("lib", "cmake", "Mdt0DeployUtils") )
+    self.env_info.PATH.append( os.path.join(self.package_folder, "bin") )
--- packaging/conan/MdtDeployUtils/test_package/conanfile.py
+++ packaging/conan/MdtDeployUtils/test_package/conanfile.py
@@ -1,24 +1,35 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 import os
+
 
 class MdtDeployUtilsTest(ConanFile):
   settings = "os", "compiler", "build_type", "arch"
-  generators = "cmake", "virtualenv"
+  generators = "CMakeDeps"
   #build_requires = "MdtCMakeModules/[0.16.0]@scandyna/testing"
 
-  def configure_cmake(self):
-    cmake = CMake(self)
 
-    cmake.definitions["CMAKE_MESSAGE_LOG_LEVEL"] = "DEBUG"
+  # In the package, dependencies are build requirements.
+  # So, we don't inherit Qt in this test package
+  def requirements(self):
+    self.requires("qt/5.15.6")
 
-    return cmake
+
+  def generate(self):
+    tc = CMakeToolchain(self)
+    tc.variables["CMAKE_MESSAGE_LOG_LEVEL"] = "DEBUG"
+    tc.generate()
+
 
   def build(self):
-      cmake = self.configure_cmake()
-      cmake.configure()
-      cmake.build()
-      cmake.install()
+    cmake = CMake(self)
+    cmake.configure()
+    cmake.build()
+    cmake.install()
+
 
   def test(self):
-      cmake = self.configure_cmake()
-      cmake.test(output_on_failure=True)
+    cmake = CMake(self)
+    cmake.test()
+    # This does not work with new CMake tool
+    #cmake.test(output_on_failure=True)
