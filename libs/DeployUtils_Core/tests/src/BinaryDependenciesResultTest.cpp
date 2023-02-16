7a47095a26d48450673c363b08ee73116fb4177e
Philippe Steinmann <philippe.steinmann@gmail.com>
2023-02-16 05:08:39 +0100

    BinaryDependenciesResultTest: remove OLD test (GL task #7)

--- libs/DeployUtils_Core/tests/src/BinaryDependenciesResultTest.cpp
+++ libs/DeployUtils_Core/tests/src/BinaryDependenciesResultTest.cpp
@@ -28,61 +28,6 @@ TEST_CASE("construct")
   REQUIRE( result.isSolved() );
 }
 
-/*
-TEST_CASE("addLibrary_OLD")
-{
-  const auto os = OperatingSystem::Linux;
-  QFileInfo file( QLatin1String("/opt/project/app") );
-  BinaryDependenciesResult result(file, os);
-
-  SECTION("add a library with its full path (found)")
-  {
-    QFileInfo libA( QLatin1String("/opt/libA.so") );
-    result.addLibrary( BinaryDependenciesFile::fromQFileInfo(libA) );
-
-    REQUIRE( result.libraryCount() == 1 );
-    REQUIRE( result.containsLibraryName( QLatin1String("libA.so") ) );
-    REQUIRE( result.isSolved() );
-  }
-
-  SECTION("add a library that was not found")
-  {
-    QFileInfo libA( QLatin1String("libA.so") );
-    result.addLibrary( BinaryDependenciesFile::fromLibraryName(libA) );
-
-    REQUIRE( result.libraryCount() == 1 );
-    REQUIRE( result.containsLibraryName( QLatin1String("libA.so") ) );
-    REQUIRE( !result.isSolved() );
-  }
-
-  SECTION("add a library 2x")
-  {
-    QFileInfo libA( QLatin1String("libA.so") );
-    result.addLibrary( BinaryDependenciesFile::fromLibraryName(libA) );
-    result.addLibrary( BinaryDependenciesFile::fromLibraryName(libA) );
-
-    REQUIRE( result.libraryCount() == 1 );
-    REQUIRE( result.containsLibraryName( QLatin1String("libA.so") ) );
-    REQUIRE( !result.isSolved() );
-  }
-
-  SECTION("add a library 2x but with different case")
-  {
-    QFileInfo libA( QLatin1String("libA.DLL") );
-    QFileInfo liba( QLatin1String("liba.dll") );
-
-    result.addLibrary( BinaryDependenciesFile::fromLibraryName(libA) );
-    result.addLibrary( BinaryDependenciesFile::fromLibraryName(liba) );
-
-    REQUIRE( result.libraryCount() == 1 );
-    REQUIRE( result.containsLibraryName( QLatin1String("liba.dll") ) );
-    REQUIRE( result.containsLibraryName( QLatin1String("libA.dll") ) );
-    REQUIRE( result.containsLibraryName( QLatin1String("libA.DLL") ) );
-    REQUIRE( !result.isSolved() );
-  }
-}
-*/
-
 TEST_CASE("addLibrary")
 {
   const auto os = OperatingSystem::Linux;
